53549834399d47e5ccd5fb5a1e8a501505e71de8
snehasish-dev <snehasishdeb439@gmail.com>
2025-04-16 08:21:55 +0530

    Add Dubule Equalto program

---
+++ Equalto_operator.py
@@ -0,0 +1,12 @@
+# Condition -1
+a = 4
+b = "4"
+print(a is b )# A and B memory location is equal than it is True
+print(a == b) # Value compare
+#Condition -2
+#Create a one memory location because a and b value is 4 
+a = 4
+b = 4
+print(a is b) #Output -True
+print(a == b)#Output -True
+
